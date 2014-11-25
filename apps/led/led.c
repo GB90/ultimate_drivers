6095f1fe457355d1c409a60eb57cb3ceaea63872
chlachof-linux <chlachof@126.com>
2014-11-25 16:37:11 +0800

    增加了总线按键

--- apps/led/led.c
+++ apps/led/led.c
@@ -56,6 +56,21 @@ int main(void)
         x_bus.u32_bus_addr = 0x02;
         x_bus.u32_bus_data = (j<<(i%8))&0xff;
         ioctl(i32_fd, UD_BUS_CMD_SET_DATA, &x_bus);
+
+        ioctl(i32_fd, UD_BUS_CMD_GET_DATA, &x_bus);
+        if(x_bus.u32_bus_data != 0xff)
+        {
+            x_bus.u32_bus_addr = 0x02;
+            x_bus.u32_bus_data = 0x83;
+            ioctl(i32_fd, UD_BUS_CMD_SET_DATA, &x_bus);
+        }
+        else
+        {
+            x_bus.u32_bus_addr = 0x02;
+            x_bus.u32_bus_data = 0x80;
+            ioctl(i32_fd, UD_BUS_CMD_SET_DATA, &x_bus);
+        }
+
         sleep(1);
     }
 
