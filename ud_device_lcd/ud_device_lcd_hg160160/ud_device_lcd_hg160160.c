8cded24307d12f78bd20ed3ec46de43234995df9
chlachof-linux <chlachof@126.com>
2014-12-04 11:00:16 +0800

    增加了复位延时

--- ud_device_lcd/ud_device_lcd_hg160160/ud_device_lcd_hg160160.c
+++ ud_device_lcd/ud_device_lcd_hg160160/ud_device_lcd_hg160160.c
@@ -3502,7 +3502,6 @@ void ud_lcd_refresh(void)
 static int __init ud_lcd160160_module_init (void)
 {
     ud_lcd_init();
-    ud_lcd_init();
     ud_lcd_refresh();
 
     printd("insmod successfully\n");
